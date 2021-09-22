03b60bc9b47d9673e0fe092ea420be1aa297a6cd
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-09-22 06:30:33 +0500

    Add clear_cart action

--- src/reducer.js
+++ src/reducer.js
@@ -1,6 +1,12 @@
 import { INCREASE, DECREASE, REMOVE, CLEAR_CART } from "./Actions";
 
 function reducer(state, action) {
+  // CLEAR_CART
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [], total_items: 0, bill: 0 };
+  }
+
+  // If no action matches, return the existing state
   return state;
 }
 
