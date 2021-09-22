e43422f56d445e452301d5de0ba342bb3f356903
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-09-22 06:59:55 +0500

    Add increase action

--- src/reducer.js
+++ src/reducer.js
@@ -6,6 +6,31 @@ function reducer(state, action) {
     return { ...state, cart: [], total_items: 0, bill: 0 };
   }
 
+  // INCREASE
+  if (action.type === INCREASE) {
+    let tempCart = state.cart;
+    let matched = false;
+
+    tempCart = state.cart.map((cartItem) => {
+      if (action.payload.id === cartItem.id) {
+        cartItem = { ...cartItem, quantity: cartItem.quantity + 1 };
+        matched = true;
+      }
+      return cartItem;
+    });
+
+    if (matched === false) {
+      tempCart = [...state.cart, action.payload];
+    }
+
+    return {
+      ...state,
+      cart: tempCart,
+      total_items: state.total_items + 1,
+      bill: state.bill + parseFloat(action.payload.price),
+    };
+  }
+
   // If no action matches, return the existing state
   return state;
 }
