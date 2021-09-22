783a858fcd1d1491b4a77f2f3a77531572721e64
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-09-22 07:24:02 +0500

    Add decrease action

--- src/reducer.js
+++ src/reducer.js
@@ -31,6 +31,31 @@ function reducer(state, action) {
     };
   }
 
+  // DECREASE
+  if (action.type === DECREASE) {
+    let tempCart = state.cart;
+
+    if (action.payload.quantity === 1) {
+      tempCart = state.cart.filter(
+        (cartItem) => cartItem.id !== action.payload.id
+      );
+    } else {
+      tempCart = state.cart.map((cartItem) => {
+        if (action.payload.id === cartItem.id) {
+          cartItem = { ...cartItem, quantity: cartItem.quantity - 1 };
+        }
+        return cartItem;
+      });
+    }
+
+    return {
+      ...state,
+      cart: tempCart,
+      total_items: state.total_items - 1,
+      bill: state.bill - parseFloat(action.payload.price),
+    };
+  }
+
   // If no action matches, return the existing state
   return state;
 }
