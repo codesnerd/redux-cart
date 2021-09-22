90cf3b2a49b3db8840c68ee41c32137472d9ab48
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-09-23 02:38:08 +0500

    Add Footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 import CartContainer from "./CartContainer";
+import Footer from "./Footer";
 
 function App() {
   return (
     <main>
       <Navbar />
       <CartContainer />
+      <Footer />
     </main>
   );
 }
---
+++ src/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>
+        <a
+          className="footer-link"
+          href="https://github.com/codesnerd/redux-cart"
+        >
+          &lt;/&gt;
+        </a>{" "}
+        with ❤️ by{" "}
+        <a className="footer-link" href="https://codesnerd.com/">
+          codesnerd
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
