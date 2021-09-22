d5cc80163b54b6625b356beb0dcadac7cf9f8344
codesnerd <70039999+codesnerd@users.noreply.github.com>
2021-09-23 01:49:09 +0500

    Remove About link

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,19 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const Navbar = ({ cart = [], total_items, bill, dispatch }) => {
+const Navbar = ({ total_items }) => {
   if (total_items === 0) {
     return (
       <div className="navbar">
         <h1 className="hero-heading">Cart is empty</h1>
-        <h2>About</h2>
       </div>
     );
   } else {
     return (
       <div className="navbar">
         <h1 className="hero-heading">{total_items} Items in the Cart</h1>
-        <h2>About</h2>
       </div>
     );
   }
